6a1cc886ec7845e96f4ba6588a3f4db34ba34247
Berkay Hasip Dural <110682297+berkayhasipdural@users.noreply.github.com>
2022-09-21 00:35:27 +0300

    Add files via upload

---
+++ 8)arraysDemo/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String ogrenci1="Berkay";
+		String ogrenci2="Hasip";
+		String ogrenci3="Dural";
+		
+		String[] ogrenciler = new String [3];
+		ogrenciler[0]="Berkay";
+		ogrenciler[1]="Hasip";
+		ogrenciler[2]="Dural";
+
+		for (int i=0;i<ogrenciler.length;i++) {
+			System.out.println(ogrenciler[i]);
+		}
+ 	}
+
+}
