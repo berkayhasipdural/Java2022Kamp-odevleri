629ccec2abbc49b21c75cc856846cccaa98647ba
Berkay Hasip Dural <110682297+berkayhasipdural@users.noreply.github.com>
2022-09-21 00:34:28 +0300

    Add files via upload

---
+++ 4)conditionalss/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 999990;
+		if(sayi<30) {
+			System.out.println("Sayý 30 dan küçüktür" );
+		}else if(sayi==30){
+			System.out.println("Sayý 30 a eþittir");
+		}else {
+				System.out.println("Sayý 30 dan büyüktür");
+		}
+
+	}
+
+}
